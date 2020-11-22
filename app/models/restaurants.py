7354eae51cc425c1f23cdbcefb7ae61e030cb7a0
mark <markmcclatchy@gmail.com>
2020-11-22 02:24:47 -0500

    gas_stations created

--- app/models/restaurants.py
+++ app/models/restaurants.py
@@ -5,8 +5,8 @@ class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, foreign_key=True)
-    place_id = db.Column(db.String(255), nullable=False)
-    cuisine_id = db.Column(db.Integer, ForeignKey='cuisine.id', nullable=False)
     name = db.Column(db.String(100), nullable=False)
+    cuisine_id = db.Column(db.Integer, ForeignKey='cuisine.id', nullable=False)
+    place_id = db.Column(db.String(255), nullable=False)
 
     stops = db.relationship('Stop', backref='restaurant', lazy='joined')
