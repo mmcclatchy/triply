2964c70cba6f3c5fd06295f9bd6d579c4c9f4388
mark <markmcclatchy@gmail.com>
2020-11-24 12:29:29 -0500

    corrected SubStop typo

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -4,7 +4,7 @@ from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
 
-auth_routes = Blueprint('auth', __name__, url_prefix='/api')
+auth_routes = Blueprint('auth', __name__)
 
 
 def validation_errors_to_error_messages(validation_errors):
