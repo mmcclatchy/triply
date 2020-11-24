01bebebb2fad2dd5086828956debde8d9f6f3546
mark <markmcclatchy@gmail.com>
2020-11-24 13:06:10 -0500

    login route verified

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -4,24 +4,26 @@ from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 
+dr = DataRequired()
+
+
 def user_exists(form, field):
     print("Checking if user exists", field.data)
-    email = field.data
-    user = User.query.filter(User.email == email).first()
+    username = field.data
+    user = User.query.filter(User.username == username).first()
     if not user:
-        raise ValidationError("Email provided not found.")
+        raise ValidationError("username provided not found.")
 
 
 def password_matches(form, field):
     print("Checking if password matches")
     password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
+    username = form.data['username']
+    user = User.query.filter(User.username == username).first()
     if not user.check_password(password):
         raise ValidationError("Password was incorrect.")
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[
-                           DataRequired(), password_matches])
+    username = StringField('username', validators=[dr, user_exists])
+    password = StringField('password', validators=[dr, password_matches])
