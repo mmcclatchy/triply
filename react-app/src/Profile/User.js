65bfc9df5056042fc43ccc2fdc3438913d590e6b
alycialwang@gmail.com <alycialwang@gmail.com>
2020-11-25 12:42:56 -0500

    User Car's Rendering on Profile

--- react-app/src/Profile/User.js
+++ react-app/src/Profile/User.js
@@ -5,7 +5,7 @@ import CarForm from './CarForm';
 
 function User() {
   const [user, setUser] = useState({});
-  const [cars, setCars] = useState({});
+  const [cars, setCars] = useState([]);
   // Notice we use useParams here instead of getting the params
   // From props.
   const { userId } = useParams();
@@ -21,16 +21,14 @@ function User() {
     })();
   }, [userId]);
 
-
-//   useEffect(() => {
-//     const getCars = async () => {
-//       const response = await fetch(`/api/users/${userId}/cars`);
-//       const cars = await response.json();
-//       setCars(cars);
-//     };
-//     getCars();
-//   });
-
+  useEffect(() => {
+    const getCars = async () => {
+      const response = await fetch(`/api/users/${userId}/cars`);
+      const data = await response.json();
+      setCars(data.cars);
+    };
+    getCars();
+  }, []);
 
   if (!user) {
     return null;
@@ -52,10 +50,25 @@ function User() {
           <strong>Email</strong> {user.email}
         </li>
       </ul>
+      <div>
+        <h3>{user.username}'s Cars</h3>
+        {cars &&
+          Object.keys(cars).map(key => {
+            const current = cars[`${key}`];
+
+            return (
+              <div key={current.id}>
+                <h4>
+                  {current.year} {current.make} {current.model} ({current.mpg}{' '}
+                  mpg)
+                </h4>
+              </div>
+            );
+          })}
+      </div>
 
       <button>Add a New Car</button>
       <CarForm />
-
     </div>
   );
 }
