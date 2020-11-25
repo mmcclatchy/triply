e4bd56bb64994da3bc1febbbdda8deac156a7a8a
Ryan Black <ryanblack045@gmail.com>
2020-11-25 11:29:43 -0500

    rendering map

--- react-app/src/Map/Map.js
+++ react-app/src/Map/Map.js
@@ -1,64 +1,21 @@
 /* global google */
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core';
 const{
 withScriptjs,
 withGoogleMap,
 GoogleMap,
 DirectionsRenderer,
 } = require("react-google-maps");
-// import{GoogleMap, withScriptjs, withGoogleMap,DirectionsRenderer} from "react-google-maps"
 const { compose, withProps, lifecycle } = require ("recompose");
 //  use withScriptjs to wrap the map in order to get the map to load correctly
-
-
-// function Map() {   //defaultZoom and defaultCenter are required. Can get default center from "start point"
-//   return (
-//     <GoogleMap
-//       defaultZoom={10}
-//       defaultCenter={{ lat: 40.991360, lng: -72.534203 }}
-//     >
-//     </GoogleMap>
-//   )
-// }
-
-// const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-// const DirectionsService = new google.maps.DirectionsService();
-
-// DirectionsService.route({
-//   origin: new google.maps.LatLng(40.991360, -72.534203),
-//   destination: new google.maps.LatLng(36.099861, -80.244217),
-//   travelMode: google.maps.TravelMode.DRIVING,
-// }, (result, status) => {
-//     if (status === google.maps.DirectionsStatus.OK) {
-//       this.setState({
-//         directions: result,
-//       });
-//     } else {
-//       console.error(`error fetching directions ${result}`)
-//     }
-// })
-
-// //make sure to create .env.local file with REACT_APP_GOOGLE_KEY ="apikey"
-//  export default function App() {
-//    return (
-//     <div style ={{width: '50vw', height: '70vh'}}>
-//       <WrappedMap
-//         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
-//         loadingElement={<div style={{ height: "100% " }} />}
-//         containerElement={<div style={{ height: "100% " }} />}
-//         mapElement={<div style={{ height: "100% "}} />}
-//       />
-//     </div>
-//   );
-// }
-
 
 const MapWithADirectionsRenderer = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `400px`, marginLeft: `500px`, width: `500px`}} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
@@ -93,10 +50,19 @@ const MapWithADirectionsRenderer = compose(
 
 
 export default function Map() {
-      return (
-        <div style ={{width: '50vw', height: '70vh'}}>
+  return (
+        <>
+          <div style ={{width: '50vw', height: '70vh'}}>
           <MapWithADirectionsRenderer />
-        </div>
+          </div>
+
+          <div>
+            <TextField id="origin" label="Origin" variant="filled" />
+            <TextField id="origin" label="Destination" variant="filled" />
+          </div>
+          <Button variant="contained">
+          Submit
+           </Button>
+        </>
       );
    }
-
