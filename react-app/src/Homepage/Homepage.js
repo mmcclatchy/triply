b12a8f33fc91fc70f3826458c949f0d3c876d15a
alycialwang@gmail.com <alycialwang@gmail.com>
2020-11-27 16:51:51 -0500

    HomePage Formatted

--- react-app/src/Homepage/Homepage.js
+++ react-app/src/Homepage/Homepage.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from 'react';
 import { authenticate } from '../services/auth';
-import Header from '../shared_components/Header';
+import NavBar from '../shared_components/NavBar';
+import './Homepage.css';
 
 const Homepage = ({ authenticated, setAuthenticated, userId }) => {
   useEffect(() => {}, []);
 
   return (
     <div>
-      <Header
-        authenticated={authenticated}
-        setAuthenticated={setAuthenticated}
-        userId={userId}
-      />
-      <h1>Welcome to Triply!</h1>
+      <div className='homepage_body'>
+        <div className='header__content'>
+          <div className='logo'></div>
+          <NavBar
+            authenticated={authenticated}
+            setAuthenticated={setAuthenticated}
+            userId={userId}
+          />
+        </div>
+        <div className='tag_container'>
+          <h1 className='homepage__tagline'>Make every </h1>
+          <h1 className='homepage__tagline'>drive an adventure</h1>
+        </div>
+      </div>
     </div>
   );
 };
