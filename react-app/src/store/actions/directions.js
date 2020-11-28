04987a4a6011cabce1101a1ab14f9360767166b7
Ryan Black <ryanblack045@gmail.com>
2020-11-28 17:59:01 -0500

    redering trip details on page

--- react-app/src/store/actions/directions.js
+++ react-app/src/store/actions/directions.js
@@ -1,8 +1,12 @@
 export const SET_ORIGIN = 'triply/directions/SET_ORIGIN';
 export const SET_DESTINATION = 'triply/directions/SET_DESTINATION';
 export const SET_START_TIME = 'triply/directions/SET_START_TIME';
+export const SET_TRIP_DURATION = 'triply/directions/SET_TRIP_DURATION';
+export const SET_TRIP_DISTANCE = 'triply/directions/SET_TRIP_DISTANCE';
 
 
 export const setOriginAction = origin => ({ type: SET_ORIGIN, origin });
-export const setDestinationAction = destination => ({ type: SET_DESTINATION, destination })
-export const setStartTimeAction = startTime => ({type: SET_START_TIME, startTime})
+export const setDestinationAction = destination => ({ type: SET_DESTINATION, destination });
+export const setStartTimeAction = startTime => ({ type: SET_START_TIME, startTime });
+export const setDistanceAction = distance => ({ type: SET_TRIP_DISTANCE, distance });
+export const setDurationAction = duration => ({ type: SET_TRIP_DURATION, duration})
--- react-app/src/store/reducers/directions.js
+++ react-app/src/store/reducers/directions.js
@@ -1,4 +1,4 @@
-import { SET_DESTINATION, SET_ORIGIN, SET_START_TIME} from '../actions/directions';
+import { SET_DESTINATION, SET_ORIGIN, SET_START_TIME,  SET_TRIP_DISTANCE, SET_TRIP_DURATION} from '../actions/directions';
 
 export default function reducer(state = {}, action) {
   switch (action.type) {
@@ -6,10 +6,16 @@ export default function reducer(state = {}, action) {
       return { ...state, origin: action.origin };
     }
     case SET_DESTINATION: {
-      return{...state, destination: action.destination}
+      return {...state, destination: action.destination}
     }
     case SET_START_TIME: {
-      return{...state, startTime: action.startTime}
+      return {...state, startTime: action.startTime}
+    }
+    case SET_TRIP_DISTANCE: {
+      return {...state, distance: action.distance}
+    }
+    case SET_TRIP_DURATION: {
+      return {...state, duration: action.duration}
     }
     default:
       return state;
