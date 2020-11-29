eef6d25c173dba5af7344f0c5e8c46cb39006084
mark <markmcclatchy@gmail.com>
2020-11-29 01:58:14 -0500

    updated gas, rest, hotel, and stop models

--- app/models/hotels.py
+++ app/models/hotels.py
@@ -6,6 +6,12 @@ class Hotel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    phone_num = db.Column(db.String(15))
+    street_address = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(2))
+    zip_code = db.Column(db.String(10))
+    img_url = db.Column(db.String(255))
     stars = db.Column(db.Float, nullable=False)
     place_id = db.Column(db.String(255), nullable=False)
 
@@ -15,6 +21,12 @@ class Hotel(db.Model):
         return {
             'id': self.id,
             'name': self.name,
+            'phone_num': self.phone_num,
+            'street_address': self.street_address,
+            'city': self.city,
+            'state': self.state,
+            'zip_code': self.zip_code,
+            'img_url': self.img_url,
             'stars': self.stars,
             'place_id': self.place_id,
             'stops': [stop.id for stop in self.stops]
